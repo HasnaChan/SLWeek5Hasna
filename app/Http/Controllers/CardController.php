ae1ec329b56f4e28850540e79db8dd2e9fd64eb6
Hasna Salsabilla <hasnas291@gmail.com>
2023-04-11 00:13:22 +0700

    done

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -20,11 +20,40 @@ class CardController extends Controller
 
     public function detail($id){
         $cards = [
-            ['id' => 0, 'ProductName' => 'Crown', 'StatusProduct' => 'SSR', 'ProductPrice' => 'Rp 5.000.000,00', 'image' => 'crown.jpg'],
-            ['id' => 1, 'ProductName' => 'Necklace', 'StatusProduct' => 'SR', 'ProductPrice' => 'Rp 4.500.000', 'image' => 'necklace.jpg'],
-            ['id' => 2, 'ProductName' => 'Ring', 'StatusProduct' => 'R', 'ProductPrice' => 'Rp 3.000.000,00', 'image' => 'ring.jpg'],
-            ['id' => 3, 'ProductName' => 'Earring', 'StatusProduct' => 'SR', 'ProductPrice' => 'Rp 3.500.000', 'image' => 'earrings.jpg'],
-            ['id' => 4, 'ProductName' => 'Ruby', 'StatusProduct' => 'SSR', 'ProductPrice' => 'Rp 10.500.000', 'image' => 'ruby.jpg']
+            ['id' => 0,
+            'ProductName' => 'Crown',
+            'StatusProduct' => 'SSR',
+            'ProductPrice' => 'Rp 5.000.000,00',
+            'image' => 'crown.jpg',
+            'ProductDesc' => 'A crown is a circular ornament, usually made of gold and jewels, which a king or queen wears on their head at official ceremonies.'],
+
+            ['id' => 1,
+            'ProductName' => 'Necklace',
+            'StatusProduct' => 'SR',
+            'ProductPrice' => 'Rp 4.500.000',
+            'image' => 'necklace.jpg',
+            'ProductDesc' => 'A necklace is an article of jewellery that is worn around the neck. Necklaces may have been one of the earliest types of adornment worn by humans.'],
+
+            ['id' => 2,
+            'ProductName' => 'Ring',
+            'StatusProduct' => 'R',
+            'ProductPrice' => 'Rp 3.000.000,00',
+            'image' => 'ring.jpg',
+            'ProductDesc' => 'A ring is a round band, usually made of metal, worn as ornamental jewelry. The term "ring" by itself always denotes jewellery worn on the finger.'],
+
+            ['id' => 3,
+            'ProductName' => 'Earring',
+            'StatusProduct' => 'SR',
+            'ProductPrice' => 'Rp 3.500.000',
+            'image' => 'earrings.jpg',
+            'ProductDesc' => 'An earring is a piece of jewelry attached to the ear via a piercing in the earlobe or another external part of the ear.'],
+
+            ['id' => 4,
+            'ProductName' => 'Ruby',
+            'StatusProduct' => 'SSR',
+            'ProductPrice' => 'Rp 10.500.000',
+            'image' => 'ruby.jpg',
+            'ProductDesc' => 'Ruby is an interpreted, high-level, general-purpose programming language which supports multiple programming paradigms.']
         ];
 
         $card = [];
--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,15 +5,35 @@
 @endsection
 
 @section('container')
-<div class="card {{ $card["StatusProduct"] }}" >
-    <img class="card-img-top" src="{{ asset('img/' . $card['image']) }}"
-    alt="{{ $card['ProductName'] }}" style="width:200px">
-    <div class="card-body">
-        <h5 class="card-title">Name: {{ $card['ProductName'] }}</h5>
-        <h6 class="card-text">Status: {{ $card['StatusProduct'] }}</h6>
-        <h6 class="card-text">Price: {{ $card['ProductPrice'] }}</h6>
-        <a href="card/{{ $card['id'] }}" class="btn btn-primary">See Detail</a>
-    </div>
-</div>
+
+{{-- <div class="row row-md-4 justify-content-around"> --}}
+
+    {{-- <div class="col-4 col-md-4 text-center" >
+            <div class="card {{ $card["StatusProduct"] }}" >
+                    <img class="card-img-top" src="{{ asset('img/' . $card['image']) }}"
+                    alt="{{ $card['ProductName'] }}" style="width:200px">
+                    <div class="card-body">
+                        <h5 class="card-title">Name: {{ $card['ProductName'] }}</h5>
+                        <h6 class="card-text">Status: {{ $card['StatusProduct'] }}</h6>
+                        <h6 class="card-text">Price: {{ $card['ProductPrice'] }}</h6>
+                        <h5 class="card-title">Description</h5>
+                        <h6 class="card-text">{{ $card['ProductDesc'] }}</h6>
+                    </div>
+            </div>
+    </div> --}}
+
+    <div class="card {{ $card["StatusProduct"] }} p-1 justify-content-around flex flex-row" style="width:80rem">
+        <img src="{{ asset('img/' . $card['image']) }}" class="card-img-top " alt="{{ $card['ProductName'] }}" style="width:200px" >
+        <div class="card-body">
+            <h5 class="card-title">Name: {{ $card['ProductName'] }}</h5>
+            <h6 class="card-text">Status: {{ $card['StatusProduct'] }}</h6>
+            <h6 class="card-text">Price: {{ $card['ProductPrice'] }}</h6>
+            <h5 class="card-title">Description</h5>
+            <h6 class="card-text">{{ $card['ProductDesc'] }}</h6>
+        </div>
+      </div>
+
+
+{{-- </div> --}}
 
 @endsection
